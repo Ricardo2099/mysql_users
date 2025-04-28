51758ab93bb61b349800cd79dd55b7799c424355
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-27 21:17:19 -0600

    para render

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
-// config/config.js
-require('dotenv').config();
+require('dotenv').config();  // Asegúrate de cargar las variables de entorno
 
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host:     process.env.DB_HOST,
-    port:     process.env.DB_PORT || 3306,
-    dialect:  process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
   },
 
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST || 'user-api-test',
-    host:     process.env.DB_HOST,
-    port:     process.env.DB_PORT || 3306,
-    dialect:  process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
   },
 
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PROD || 'user-api-prod',
-    host:     process.env.DB_HOST,
-    port:     process.env.DB_PORT || 3306,
-    dialect:  process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
   }
 };
+
